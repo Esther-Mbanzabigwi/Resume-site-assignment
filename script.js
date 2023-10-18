8e95e6c4a16dfd981456b1dde575f0742a01949d
Esther <e.mbanzabig@alustudent.com>
2023-10-18 15:22:40 +0200

    design response

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add JavaScript interactivity here, e.g., form validation or image sliders
+document.addEventListener("DOMContentLoaded", function () {
+    // Code to run after the page is fully loaded
+    // For example, you can add form validation logic here.
+});
